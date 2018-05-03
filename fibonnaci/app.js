57bb11110b3f51dad2340478ed1c53619d686c7e
Anthony Gauthier <antho325@hotmail.com>
2018-05-03 13:55:25 -0400

    iterate and calculate fibonnaci + output the sum for a specific number

--- fibonnaci/app.js
+++ fibonnaci/app.js
@@ -5,12 +5,13 @@ const argv = yargs.argv;
 const number = argv._[0];
 
 
-function fibonnaci(n) {
-    var a = 0,
+let calc_fibonnaci = (n) => {
+    let a = 0,
         b = 0,
+        array = [],
         currentValue = 0;
 
-    for(var i=1; i < n; i++) {
+    for(let i=1; i < n; i++) {
         if(a == 0) {
             a = 1;
         } else {
@@ -19,8 +20,31 @@ function fibonnaci(n) {
         }
 
         currentValue = a + b;
-        console.log(currentValue);
+        array.push(currentValue);
     }
+    console.log(`Iterating fibonnaci ${n} times: ${array}`);
 }
 
-fibonnaci(number);
+let outputSum_fibonnaci = (n) => {
+    let a = 0,
+        b = 0,
+        currentValue = 0;
+    
+    while(currentValue != n) {
+        if(a == 0) {
+            a = 1;
+        } else {
+            b = a;
+            a = currentValue
+        }
+        currentValue = a + b;
+    }
+
+    console.log(`The fibonnaci sum for ${n} = ${b} + ${a}`);
+}
+
+console.log('---------------------------------------------------------');
+calc_fibonnaci(number);
+console.log('---------------------------------------------------------');
+outputSum_fibonnaci(number);
+console.log('---------------------------------------------------------');
