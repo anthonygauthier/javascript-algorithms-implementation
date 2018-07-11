4137b6330196929da430f98830af416f93bd9c40
Anthony Gauthier <antho325@hotmail.com>
2018-07-11 14:00:04 -0400

    SelSearch - removed bool 'found'

--- searches/selectionSeach.js
+++ searches/selectionSeach.js
@@ -1,28 +1,25 @@
 let selectionSearch = (x) => {
   let array = [3,6,9,12,15,45,52,78,92,120]
-    , found = false
     , message = ''
     , midValueIndex = Math.round((array.length - 1) / 2);
 
   for(let i = 0; i < array.length; i++) {
     if(x > array[array.length - 1]) {
-      return found;
+      return false
     } else if(x === array[i]) {
-      found = true;
-      break;
+      return true
     } else {
       if(x < array[midValueIndex]) {
         midValueIndex = Math.round(midValueIndex / 2);
       } else if(x > array[midValueIndex]) {
         midValueIndex += Math.round(((array.length -1) - midValueIndex) / 2);
       } else {
-        found = true;
-        break;
+        return true;
       }
     }
   }
 
-  return found;
+  return false;
 }
 
 module.exports = {
